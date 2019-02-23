96bfae0029a0ae6a633a333f012e552ddee33dc1
Lenox89 <cameron.macon@gmail.com>
2019-02-23 14:12:10 -0800

    Styled player
    Added responsive styling.

--- app/js/app.js
+++ app/js/app.js
@@ -20,15 +20,22 @@ app.controller('homeController',function($scope){
 
 
   for (var i = 0; i <= audios.length - 1; i++) {
-    player.append('<li>'+audios[i].title+'<br/><audio src="'+audios[i].src+'" controls></audio></li>');
+    player.append('<li class="list-group-item">'+audios[i].title+'<br/><audio src="'+audios[i].src+'" controls></audio></li>');
   }
 
   $('audio').each(function(index,el){
     el.addEventListener('play', function() { 
       $('audio').each(function(index,ol){
+        $(ol).parent().addClass('active');
         if(el != ol){
           ol.load(); 
+          $(ol).parent().removeClass('active');
         }
+      });
+    }, true);
+    el.addEventListener('ended', function() { 
+      $('audio').each(function(index,ol){
+        $(ol).parent().removeClass('active');
       });
     }, true);
   })
