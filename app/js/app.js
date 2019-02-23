9639cc96436f0ca88b4ab6be0233ad1839cb13e1
Lenox89 <cameron.macon@gmail.com>
2019-02-23 02:25:21 -0800

    Created header

--- app/js/app.js
+++ app/js/app.js
@@ -9,21 +9,22 @@ app.config(function($routeProvider,$locationProvider){
   });
 });
 
-app.controller("homeController",function($scope){
+app.controller('homeController',function($scope){
+
   var player = $('#player');
+
   var audios = [{src:'media/audio/SampleAudio_0.4mb.mp3',
                  title:'Sample 1'},
                 {src:'media/audio/SampleAudio_0.7mb.mp3',
                  title:'Sample 2'}]
 
-  $scope.audios = audios;
 
   for (var i = 0; i <= audios.length - 1; i++) {
     player.append('<li>'+audios[i].title+'<br/><audio src="'+audios[i].src+'" controls></audio></li>');
   }
 
   $('audio').each(function(index,el){
-    el.addEventListener("play", function() { 
+    el.addEventListener('play', function() { 
       $('audio').each(function(index,ol){
         if(el != ol){
           ol.load(); 
@@ -31,4 +32,15 @@ app.controller("homeController",function($scope){
       });
     }, true);
   })
+});
+
+app.controller('navController',function($scope){
+  $scope.links =[{url:'/link1',
+                  title:'Link 1'},
+                 {url:'/link2',
+                  title:'Link 2'},
+                 {url:'/link3',
+                  title:'Link 3'},
+                 {url:'/link4',
+                  title:'Link 4'}]
 });
